12f445f165de2b4c95b0e1b869e4e40d1a777ea5
Uthkarsh <uthkarshkrish2302@gmail.com>
2026-06-05 10:52:42 +0530

    solved 217, 242, 1, 49 - arrays and hashing

---
+++ 1_two_sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n=len(nums)
+        dict1={}
+        for i,n in enumerate(nums):
+            s=target-n
+            if s in dict1:
+                return [dict1[s],i]
+            dict1[n]=i
+
+        
