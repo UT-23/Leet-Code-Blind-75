0799c7de24588a9ec22c859d20b55bdb7a1148d1
Uthkarsh <uthkarshkrish2302@gmail.com>
2026-06-17 11:53:00 +0530

    121 643 1456 - solved sliding window fixed size

---
+++ Sliding_Window/1456_maximum_vowels_in_substring.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxVowels(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        vowels="aeiou"
+        current_count = sum(1 for char in s[0:k] if char in vowels)
+        max_count=current_count
+        for i in range(k,len(s)):
+            if s[i] in vowels:
+                current_count+=1
+            if s[i-k] in vowels:
+                current_count-=1
+            max_count=max(max_count,current_count)
+        return max_count
+                
+        
